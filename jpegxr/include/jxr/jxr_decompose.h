ae2d427b409636c6faf93ace62ba9412a57811f6
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2014-01-05 23:29:39 +0200

    added split kernel for high pass and low pass coefficients

---
+++ jpegxr/include/jxr/jxr_decompose.h
@@ -0,0 +1,38 @@
+#ifndef __jxr_decompose_h__
+#define __jxr_decompose_h__
+
+#include <cstdint>
+#include <jxr/jxr_filter.h>
+
+namespace jpegxr
+{
+    namespace decompose
+    {
+        //gather dc coefficients after the pct4x4 transform
+        __global__ void split_lp_hp( const transforms::pixel* in, transforms::pixel* out, const uint32_t in_pitch, const uint32_t width, const uint32_t height, const uint32_t out_pitch)
+        {
+            auto x = blockIdx.x * blockDim.x + threadIdx.x;
+            auto y = blockIdx.y * blockDim.y + threadIdx.y;
+
+            auto row = 4 * y;
+            auto col = 4 * x;
+            auto pitch = in_pitch;
+
+            //out of image bounds
+            if ( col >= width || row >= height )
+            {
+                return;
+            }
+
+            auto element_in_index  = (row + 0) * pitch + col + 0;
+
+            auto dc = in[ element_in_index ];
+
+            auto element_out_index  = (y + 0) * out_pitch + x + 0;
+            out[ element_out_index ] = dc;
+        }
+    }
+}
+
+#endif
+
