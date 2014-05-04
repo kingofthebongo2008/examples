0d81c4800db828ea15c4484ad446fc9157bdd2dc
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2014-05-04 14:44:19 +0300

    added svd_rotation.h

--- wavelet_spline/include/svd/svd.h
+++ wavelet_spline/include/svd/svd.h
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <tuple>
 
+#include "svd_types.h"
 #include "svd_math.h"
 
 /*
@@ -19,58 +20,6 @@ Notes: For production usage please consider inlining all functions into one. Thi
 
 namespace svd
 {
-    template <typename t> struct matrix3x3
-    {
-        //row major
-        t a11; t a12; t a13;
-        t a21; t a22; t a23;
-        t a31; t a32; t a33;
-
-    };
-
-    template <typename t> struct symmetric_matrix3x3
-    {
-        //row major
-        t a11; //t a12; t a13;
-        t a21; t a22; //t a23;
-        t a31; t a32; t a33;
-    };
-
-    template <typename t> struct quaternion
-    {
-        t x;
-        t y;
-        t z;
-        t w;
-    };
-
-    template <typename t> struct vector3
-    {
-        t x;
-        t y;
-        t z;
-    };
-
-    template < typename t> inline matrix3x3<t> create_matrix 
-        ( 
-            t a11, t a12, t a13,
-            t a21, t a22, t a23,
-            t a31, t a32, t a33
-        )
-    {
-        using namespace svd::math;
-        matrix3x3<t> r =  { a11, a12, a13, a21, a22, a23, a31, a32, a33 };
-        return r;
-    }
-
-    template <typename t> inline quaternion<t> create_quaternion( t qx, t qy, t qz, t qw)
-    {
-        using namespace svd::math;
-
-        quaternion<t> r = { qx, qy, qz, qw };
-        return r;
-    }
-
     template <typename t> inline std::tuple< t, t > approximate_givens_quaternion( t a11, t a12, t a22 )
     {
         using namespace math;
--- wavelet_spline/include/svd/svd_rotation.h
+++ wavelet_spline/include/svd/svd_rotation.h
@@ -2,10 +2,14 @@
 #define __svd_svd_rotation_h__
 
 #include "svd_math.h"
+#include "svd_types.h"
 
 namespace svd
 {
+    template <typename t> inline void rotation ( const vector3<t>& p, const vector<3>& q, matrix3x3<t>& r, vector3<t>& t )
+    {
 
+    }
 }
 
 
