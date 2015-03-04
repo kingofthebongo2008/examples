48da6f35679d01163487f69fbaa16d6d3516779b
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-03-04 06:47:38 +0200

    added simple header

--- web/ColorYourWay/ColorYourWay/Site.fs
+++ web/ColorYourWay/ColorYourWay/Site.fs
@@ -15,12 +15,41 @@ module Site =
                 Div [Text title]
             ]
 
+    let ( => ) text url =
+        A [HRef url] -< [Text text]
+
+    let makeHeader ctx =
+        [
+            
+                    UL [Class "nav nav-pills"] -<
+                    [
+                        LI [ "Digital Papers" => ctx.Link DigitalPapers ]
+                        LI [ "Clip Art"       => ctx.Link ClipArt       ]
+                        LI [ "Digital Stamps" => ctx.Link DigitalStamps ]
+                        LI [ "Alphabets"      => ctx.Link Alphabets     ]
+                        LI [ "Printables"     => ctx.Link Printables    ]
+                        LI [ "Discounts"      => ctx.Link Discounts     ]
+                        LI [ "Freebies"       => ctx.Link Freebies      ]
+                    ]
+        ]
+
+    let homeBody ctx =
+        let r = [
+                    Div [Text "HOME"]
+                ]
+        r
+
+    let combine ( f :  Context<Action> -> Element list )   ( g :  Context<Action> -> Element list )  ( ctx : Context<Action> ) : Element list  =
+        let a = f ctx
+        let b = g ctx
+        List.concat [ a ; b ]
+
     let homePage =
         let title = makePageTitle ""
-        Skin.WithTemplate title <| fun ctx ->
-            [
-                Div [Text "HOME"]
-            ]
+        let header = makeHeader
+        let f = combine makeHeader homeBody
+        Skin.WithTemplate title f
+
 
     let digitalPapersPage = makePage "Digital Papers"
     let clipArtPage = makePage "Clip Art"
@@ -33,6 +62,7 @@ module Site =
     let encodeSectionUrl (s : string ) =
         let r = s.Replace(' ','-')
         r
+
     let encodeSection (name : string) =
         let e = encodeSectionUrl name;
         let r = "/" + e;
@@ -44,7 +74,7 @@ module Site =
 
     let Main =
         Sitelet.Sum [
-            content "/" Home homePage
+            content "" Home homePage
             content "Digital Papers" DigitalPapers digitalPapersPage
             content "Clipart" ClipArt clipArtPage
             content "Digital Stamps" DigitalStamps digitalStampsPage
