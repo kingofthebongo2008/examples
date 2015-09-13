687805f4f354770552659238a476c0a67828537e
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2015-09-13 19:31:44 +0300

    added

---
+++ cache_aware_hybrid_sorter/src/app/private/main.cpp
@@ -0,0 +1,11 @@
+#include "precompiled.h"
+#include <cstdint>
+
+int32_t wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR    lpCmdLine, int       nCmdShow )
+{
+    return 0;
+}
+
+
+
+
